2364463f2aa9ee83c10f1ee92978389dc9e5a5dc
Cabdalle Moha <abdallamoha243@gmail.com>
2026-03-29 23:41:56 -0700

    login Auth

--- backend/Authentication/Auth.js
+++ backend/Authentication/Auth.js
@@ -52,3 +52,35 @@ export const register = async (req, res) =>{
         res.status(500).json({ message: "Server error" });
     }
 }
+
+// login
+
+export const login = async (req,res) =>{
+    const {username,password} = req.body;
+
+    if(!username || !password){
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
+    try {
+        const user = await User.findOne ({ username});
+        if(!user){
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid){
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        generateTokenAndCookies(user._id, res);
+        res.status(200).json({ message: "Login successful",
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+         });
+         
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+}
--- backend/routes/Authroutes.js
+++ backend/routes/Authroutes.js
@@ -1,8 +1,9 @@
 import Router from "express";
-import { register } from "../Authentication/Auth.js";
+import { login, register } from "../Authentication/Auth.js";
 
 const router = Router();
 
 router.post("/register", register);
+router.post("/login", login);
 
 export default router 
