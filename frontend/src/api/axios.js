ffa835e0f484b76499b4d14901d90188f21d2c00
Cabdalle Moha <abdallamoha243@gmail.com>
2026-05-03 08:36:42 -0700

    fix: frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,19 @@
+// import axios from 'axios';
+
+// const API = axios.create({
+//   // Vite wuxuu si automatic ah u soo qaadanayaa variable-ka aad sare ku qortay
+//   baseURL: import.meta.env.VITE_API_URL, 
+//   withCredentials: true 
+// });
+
+// export default API;
+
+
 import axios from 'axios';
 
 const API = axios.create({
-  // Vite wuxuu si automatic ah u soo qaadanayaa variable-ka aad sare ku qortay
-  baseURL: import.meta.env.VITE_API_URL, 
+  // Tani waxay soo qaadanaysaa URL-ka aad Vercel gelisay
+  baseURL: import.meta.env.VITE_API_URL + "/api", 
   withCredentials: true 
 });
 
