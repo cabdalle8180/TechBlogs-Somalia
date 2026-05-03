8cf11896edd929d1fa573ee28021785b536c00d8
Cabdalle Moha <abdallamoha243@gmail.com>
2026-05-03 08:03:09 -0700

    fix: move  url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  // URL-kan waa kan rasmiga ah ee aad Railway ka heshay
-  baseURL: "https://dazzling-spontaneity-production-04e3.up.railway.app/api", 
+  // Vite wuxuu si automatic ah u soo qaadanayaa variable-ka aad sare ku qortay
+  baseURL: import.meta.env.VITE_API_URL, 
   withCredentials: true 
 });
 
